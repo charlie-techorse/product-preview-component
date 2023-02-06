b93c734ff2592b5e2fdf06365c7fc6f4f75df213
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-06 19:15:54 +0100

    Make the card component reusable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,22 @@
 import './card.css'
 
-export function Card() {
+type CardProps = {
+  image: string,
+  type: string,
+  title: string,
+  description: string,
+  price: number,
+  newPrice: number
+}
+
+export function Card({ image, type, title, description, price, newPrice }: CardProps) {
   return <div className='card'>
-    <img className='card__img' src='/image-product-mobile.jpg'></img>
-    <h2>Parfume</h2>
-    <h1>Gabrielle Essence Eau De Parfum</h1>
-    <p>
-      A floral, solar and voluptuous interpretation composed by Olivier Polge,
-      Perfumer-Creator for the House of CHANEL.
-    </p>
-    <p>$149.99</p>
-    <p>$169.99</p>
+    <img className='card__img' src={image}></img>
+    <h2>{type}</h2>
+    <h1>{title}</h1>
+    <p>{description}</p>
+    <p>${newPrice}</p>
+    <p>${price}</p>
     <button>Add to Cart</button>
   </div>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Card />
+    <Card
+      image='/image-product-mobile.jpg'
+      type='Perfume'
+      title='Gabrielle Essence Eau De Parfum'
+      description='A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.'
+      price={169.99}
+      newPrice={149.99} />
   </React.StrictMode>,
 )
