a820053478f700e8a800f2e1b27cb20afec906c6
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-06 19:08:31 +0100

    Add card text

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,5 +3,14 @@ import './card.css'
 export function Card() {
   return <div className='card'>
     <img className='card__img' src='/image-product-mobile.jpg'></img>
+    <h2>Parfume</h2>
+    <h1>Gabrielle Essence Eau De Parfum</h1>
+    <p>
+      A floral, solar and voluptuous interpretation composed by Olivier Polge,
+      Perfumer-Creator for the House of CHANEL.
+    </p>
+    <p>$149.99</p>
+    <p>$169.99</p>
+    <button>Add to Cart</button>
   </div>
 }
