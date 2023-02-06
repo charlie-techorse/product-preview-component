f3bd2592d0c730ba811684bc4d06633b29d1afec
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-06 20:06:41 +0100

    Create image type

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,8 @@
+import { ImageType } from '../types/imageType'
 import './card.css'
 
 type CardProps = {
-  image: string,
+  image: ImageType,
   type: string,
   title: string,
   description: string,
@@ -11,7 +12,7 @@ type CardProps = {
 
 export function Card({ image, type, title, description, price, newPrice }: CardProps) {
   return <div className='card'>
-    <img className='card__img' src={image}></img>
+    <img className='card__img' src={image.src} alt={image.name}></img>
     <div className='card__content'>
       <h2 className='card__content-type'>{type}</h2>
       <h1 className='card__content-title'>{title}</h1>
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Card } from './components/Card'
 import './index.css'
+import { ImageType } from './types/imageType'
+
+const image: ImageType = { src: '/image-product-mobile.jpg', name: '' }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Card
-      image='/image-product-mobile.jpg'
+      image={image}
       type='Perfume'
       title='Gabrielle Essence Eau De Parfum'
       description='A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.'
