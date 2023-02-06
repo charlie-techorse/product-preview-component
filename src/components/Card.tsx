b526781b7a02e7c7c93bd33736a38b50beb09198
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-06 19:17:53 +0100

    Give card content some padding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,11 +12,13 @@ type CardProps = {
 export function Card({ image, type, title, description, price, newPrice }: CardProps) {
   return <div className='card'>
     <img className='card__img' src={image}></img>
-    <h2>{type}</h2>
-    <h1>{title}</h1>
-    <p>{description}</p>
-    <p>${newPrice}</p>
-    <p>${price}</p>
-    <button>Add to Cart</button>
+    <div className='card__content'>
+      <h2>{type}</h2>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <p>${newPrice}</p>
+      <p>${price}</p>
+      <button>Add to Cart</button>
+    </div>
   </div>
 }
