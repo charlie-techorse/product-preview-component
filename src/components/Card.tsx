5738f35b33ac17dbc8030b76ea4ed16f4ad24f07
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-06 21:19:13 +0100

    Add desktop styling
    Also made the image a background instead

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,16 +7,18 @@ type CardProps = {
 
 export function Card({ item }: CardProps) {
   return <div className='card'>
-    <img className='card__img' src={item.image.src} alt={item.image.name}></img>
+    <div className='card__img'></div>
     <div className='card__content'>
-      <h2 className='card__content-type'>{item.type}</h2>
-      <h1 className='card__content-title'>{item.title}</h1>
-      <p className='card__content-description'>{item.description}</p>
-      <div className='card__content-prices'>
-        <p className='card__content-price card__content-new'>${item.newPrice}</p>
-        <p className='card__content-price card__content-old'>${item.price}</p>
+      <div className='card__content-wrapper'>
+        <h2 className='card__content-type'>{item.type}</h2>
+        <h1 className='card__content-title'>{item.title}</h1>
+        <p className='card__content-description'>{item.description}</p>
+        <div className='card__content-prices'>
+          <p className='card__content-price card__content-new'>${item.newPrice}</p>
+          <p className='card__content-price card__content-old'>${item.price}</p>
+        </div>
+        <button className='card__content-button'><img src="/icon-cart.svg" />Add to Cart</button>
       </div>
-      <button className='card__content-button'><img src="/icon-cart.svg" />Add to Cart</button>
     </div>
   </div>
 }
